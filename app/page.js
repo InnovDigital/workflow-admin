f76cfd6db0a4ed9f6df85a5ba1c3cef6d06a55d6
akhribabderahmane <la_akhrib@esi.dz>
2025-05-03 12:09:24 +0200

    sss

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
-  <div className="text=3xl font-bold underline text-pink-500">
-    dashboard home
-  </div>
-  );
+    <div className="text-3xl font-bold underline text-pink-500">
+      <p>Dashboard Home</p>
+      <div className="mt-4 space-x-4">
+        <Link href="/dashboard">
+          <a className="text-blue-500 hover:underline">Go to Dashboard</a>
+        </Link>
+        <Link href="/login">
+          <a className="text-blue-500 hover:underline">Go to Login</a>
+        </Link>
+      </div>
+    </div>
+  )
 }
